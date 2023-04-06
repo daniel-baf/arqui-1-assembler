b18a92c616d247ef8bd6148c25d7720e0a92d6ae
Daniel Bautista <danibaufu@gmail.com>
2023-04-06 17:23:32 -0600

    hola v4, includes

--- clases/hola/hola.asm
+++ clases/hola/hola.asm
@@ -4,13 +4,14 @@
 ; Ejemplo de impresion en pantalla con uso de archivos de cabecera
 
 ; INICIO DE PROGRAMA
-%include 'stdio32.asm'
+%include '../../utils/stdio32.asm'
 
 SECTION .data
     ; nota = esto genera un error, porque el metodo de stdio32 busca el caracter 0 en la cadena y en ningun momento hemos generado
     ; el caracter 0, por eso agregamos 0H
-    msg1 db     "Hola Arquitectura II!", 0AH, 0H ; msg = Hola arqu...
-    msg2 db     "Reutilizando funciones...", 0AH, 0H ;
+    msg1 db     "Hola Arquitectura II!", 0H ; msg = Hola arqu...
+    msg2 db     "Reutilizando funciones...", 0H ;
+    msg3 db     "Otra linea [DEBERIA SALIR SIN SALTO DE LINEA]", 0H
 
 SECTION .text
 
@@ -18,7 +19,9 @@ global _start
 
 _start:
     mov eax, msg1
-    call printStr
+    call println
     mov eax, msg2
-    call printStr
-    call endP
+    call print
+    mov eax, msg3
+    call println    
+    call sys_exit
